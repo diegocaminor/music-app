82373b6ce5f1746051d37e5e705d89465ed41b53
Diego Camino <diego.camino.r@gmail.com>
2020-06-29 00:00:12 -0500

    Test 2

---
+++ src/components/PxHeader.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  .container
+    .header
+      a.header__thumbnail
+        img.header__img(src="@/assets/images/movie-logo.png")
+      input.input.header__input
+      div.label.header__user
+        img.label.header__user--username
+        label.label.header__user--image
+</template>
+
+<script>
+export default {
+  name: "PxHeader"
+};
+</script>
+
+<style lang="scss" scoped>
+// .header__img {
+//   height: 200px;
+// }
+</style>
--- src/components/PxLogin.vue
+++ src/components/PxLogin.vue
@@ -13,7 +13,7 @@
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: "PxLogin",
   props: {
     msg: String
   }
