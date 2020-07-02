b3fc50c8aeb2629d13ad652108599bc8bf5d72fe
Diego Camino <diego.camino.r@gmail.com>
2020-07-01 21:59:10 -0500

    Now config and deploy

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -5,22 +5,22 @@
         .container
           .nav-left
             .nav-item
-              strong 📻  Platzi Music
+              strong 📻  Music App
             .nav-right.nav-menu
               router-link.nav-item(:to="{ name: 'search' }") Buscar
               router-link.nav-item(to="about") Nosotros
 
     .hero-body
       .container.has-text-centered
-        h1.title Platzi Music
+        h1.title Music App
         h2.subtitle Canciones que estan Vue-nisimas!
         pm-player
 </template>
 
 <script>
-import PmPlayer from '@/components/Player.vue'
+import PmPlayer from "@/components/Player.vue";
 
 export default {
-  components: { PmPlayer }
-}
+  components: { PmPlayer },
+};
 </script>
